ce6f08e06c4e5b1e31ce21cd3dc25e8cc8ed4e0e
mohammed-alkhateeb <101655920+mohammed-alkhateeb@users.noreply.github.com>
2023-07-30 17:21:50 +0300

    remove device_id_package

--- lib/Utils/device_utils.dart
+++ lib/Utils/device_utils.dart
@@ -1,20 +1,17 @@
 
 
-  import 'dart:io';
+import 'dart:io';
 
-import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter_udid/flutter_udid.dart';
 
 class DeviceUtils{
   static Future<String?> getDeviceId() async {
-    var deviceInfo = DeviceInfoPlugin();
-    if (Platform.isIOS) { // import 'dart:io'
-      var iosDeviceInfo = await deviceInfo.iosInfo;
-      return iosDeviceInfo.identifierForVendor; // unique ID on iOS
-    } else if(Platform.isAndroid) {
-      var androidDeviceInfo = await deviceInfo.androidInfo;
-      return androidDeviceInfo.androidId; // unique ID on Android
+    try{
+      return await FlutterUdid.udid;
     }
-    return null;
+    catch(_){
+      return null;
+    }
   }
 
 }
